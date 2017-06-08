e03cf9b56e56fbae052b074959d7c03dcd66d526
Jenia Brook <jeniabrook@gmail.com>
2017-06-08 19:41:11 +0300

    Tiles component added

---
+++ src/components/Tiles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Tiles extends React.Component {
+  render(){
+    return(
+      <div className="tiles-main-container">
+        Hi I'm Tiles Component!
+      </div>
+    );
+  }
+}
+
+export default Tiles;
