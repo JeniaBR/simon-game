5e81955899f23d2dcbc36034f00d103ed5199d9d
Jenia Brook <jeniabrook@gmail.com>
2017-06-14 22:44:38 +0300

    Add some state and generating random sequence

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import Tiles from './components/Tiles';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      myTurn: false,
+      power: false,
+      strictMode: false,
+      startGame: false,
+      simonSequence: [],
+      playerSequence: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      simonSequence: this.generateRandomSequence()
+    });
+  }
+
+  handleStartGame = () => {
+    this.setState({
+      startGame: true
+    });
+  }
+
+  generateRandomSequence = () => {
+    let randomArray = [];
+    
+    for(let i=0; i < 20 ; i++){
+      randomArray[i] = Math.floor(Math.random() * 4) + 1;
+    }
+    return randomArray;
+  }
+
   render() {
     return (
       <div className="App">
         <Tiles/>
+        <button onClick={() => this.handleStartGame()}>Start!</button>
       </div>
     );
   }
