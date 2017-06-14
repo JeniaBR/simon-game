776bb3454368b103e476851a4cfec3649fe0aaf8
Jenia Brook <jeniabrook@gmail.com>
2017-06-14 21:14:12 +0300

    basic grid layout for the game add sound and onClick event to test the sound

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,10 +1,17 @@
 import React from 'react';
+let greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+let redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+let yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+let blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
 class Tiles extends React.Component {
   render(){
     return(
       <div className="tiles-main-container">
-        Hi I'm Tiles Component!
+        <div className="green-tile" onClick={()=>greenSound.play()}></div>
+        <div className="red-tile" onClick={()=>redSound.play()}></div>
+        <div className="yellow-tile" onClick={()=>yellowSound.play()}></div>
+        <div className="blue-tile" onClick={()=>blueSound.play()}></div>
       </div>
     );
   }
